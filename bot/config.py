4e68172c692416f2e2cb6ec102b8a3895b3f2436
Arwichok <arwichok@gmail.com>
2019-09-28 21:38:05 +0300

    g to getenv

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 import ssl
-from os import getenv as g
+from os import getenv
 
 from dotenv import load_dotenv
 from aiohttp import BasicAuth
@@ -7,23 +7,23 @@ from aiohttp import BasicAuth
 
 load_dotenv()
 
-BOT_TOKEN = g('BOT_TOKEN')
-SKIP_UPDATES = bool(g('SKIP_UPDATES') or True)
-BOT_OWNER = int(g('BOT_OWNER') or 0)
+BOT_TOKEN = getenv('BOT_TOKEN')
+SKIP_UPDATES = bool(getenv('SKIP_UPDATES') or True)
+BOT_OWNER = int(getenv('BOT_OWNER') or 0)
 
-PROXY_URL = g('PROXY_URL')
-PROXY_PASS = g('PROXY_PASS', '')
-PROXY_LOGIN = g('PROXY_LOGIN', '')
+PROXY_URL = getenv('PROXY_URL')
+PROXY_PASS = getenv('PROXY_PASS', '')
+PROXY_LOGIN = getenv('PROXY_LOGIN', '')
 PROXY_AUTH = BasicAuth(PROXY_LOGIN, PROXY_PASS)
 
-USE_WEBHOOK = bool(g('USE_WEBHOOK') or False)
-WEBHOOK_HOST = g('WEBHOOK_HOST')
-WEBHOOK_PORT = int(g('WEBHOOK_PORT') or 8443)
-WEBHOOK_PATH = g('WEBHOOK_PATH')
-APP_PORT = int(g('APP_PORT') or WEBHOOK_PORT)
-APP_HOST = g('APP_HOST', 'localhost')
-SSL_CERT = g('SSL_CERT')
-SSL_KEY = g('SSL_KEY')
+USE_WEBHOOK = bool(getenv('USE_WEBHOOK') or False)
+WEBHOOK_HOST = getenv('WEBHOOK_HOST')
+WEBHOOK_PORT = int(getenv('WEBHOOK_PORT') or 8443)
+WEBHOOK_PATH = getenv('WEBHOOK_PATH')
+APP_PORT = int(getenv('APP_PORT') or WEBHOOK_PORT)
+APP_HOST = getenv('APP_HOST', 'localhost')
+SSL_CERT = getenv('SSL_CERT')
+SSL_KEY = getenv('SSL_KEY')
 
 WEBHOOK_URL = f'https://{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_PATH}'
 WEBHOOK_SERVER = {
